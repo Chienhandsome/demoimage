141c75470b6fc2b78409d291fe59879d0f2e5da3
Chienhandsome <cerkvena291@gmail.com>
2023-04-25 21:53:49 +0700

    sau khi thay đổi

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,6 +1,5 @@
 package com.example.myapplication;
 
-
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -21,7 +20,7 @@ import java.net.URL;
 public class MainActivity extends AppCompatActivity {
     Button btn;
     ImageView img;
-
+    String link="https://images.theconversation.com/files/393210/original/file-20210401-13-z6rl6z.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=1200&h=1200.0&fit=crop";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,37 +28,12 @@ public class MainActivity extends AppCompatActivity {
 
         btn = findViewById(R.id.button);
         img = findViewById(R.id.imageView);
-        // chiến mặt abc
-        // chiến mặt thộn ộn ộn nn
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                new LoadImageInterne().execute("https://images.theconversation.com/files/393210/original/file-20210401-13-z6rl6z.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=1200&h=1200.0&fit=crop");
+                Picasso.get().load("link").into(img);
             }
         });
     }
-
-    private class LoadImageInterne extends AsyncTask<String, Void, Bitmap> {
-        Bitmap bitmapImg = null;
-
-        @Override
-        protected Bitmap doInBackground(String... strings) {
-            try {
-                URL url = new URL(strings[0]);
-                InputStream inputStream = url.openConnection().getInputStream();
-                bitmapImg = BitmapFactory.decodeStream(inputStream);
-            } catch (MalformedURLException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
-            return bitmapImg;
-        }
-        @Override
-        protected void onPostExecute(Bitmap bitmap) {
-            super.onPostExecute(bitmap);
-            img.setImageBitmap(bitmap);
-        }
-    }
 }
+
